faf94274b8ddfb546a8f43d4fd713ec753ea6990
Madiocre <wolvirex@gmail.com>
2024-09-05 01:47:47 +0300

    Helper Function Fix + controllers

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+// import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -34,29 +34,29 @@ const routeController = (app) => {
     UsersController.getMe(req, res);
   });
 
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
+  // router.post('/files', (req, res) => {
+  //   FilesController.postUpload(req, res);
+  // });
 
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
+  // router.get('/files/:id', (req, res) => {
+  //   FilesController.getShow(req, res);
+  // });
 
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
+  // router.get('/files', (req, res) => {
+  //   FilesController.getIndex(req, res);
+  // });
 
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
+  // router.put('/files/:id/publish', (req, res) => {
+  //   FilesController.putPublish(req, res);
+  // });
 
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
+  // router.put('/files/:id/unpublish', (req, res) => {
+  //   FilesController.putUnpublish(req, res);
+  // });
 
-  router.post('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
+  // router.post('/files/:id/data', (req, res) => {
+  //   FilesController.getFile(req, res);
+  // });
 };
 
 export default routeController;
